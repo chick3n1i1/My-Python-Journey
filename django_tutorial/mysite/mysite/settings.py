9ec1dfe224e2387a21c0d13fbbe5dac066cd780c
chick3n1i1 <105393860+chick3n1i1@users.noreply.github.com>
2024-02-26 14:55:35 -0500

    django tutorial
    finished parts 1-3

--- django_tutorial/mysite/mysite/settings.py
+++ django_tutorial/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)91l6)f^^297(me3+*0ooe2+!=^xqzuup8ox@yw2&!%^%7*0pg'
+SECRET_KEY = 'django-insecure-+p*_jx3is#t@^jg@12@y*rmm)n1$q*cc!unf=l2ow%t*u4*r=0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
